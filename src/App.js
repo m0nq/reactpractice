023822da17f57d92b3bb7237084aa1656f07cae3
m0nq <forgetmenaught@gmail.com>
2018-08-13 16:21:07 -0700

    Add multiple components to main rendering engine

--- src/App.js
+++ src/App.js
@@ -6,8 +6,29 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h2>Welcome to this app!</h2>
-                <img src={logo}/>
+                <Header></Header>
+                <Content></Content>
+            </div>
+        );
+    }
+}
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>Some content text, yo!</p>
             </div>
         );
     }
