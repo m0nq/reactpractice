542bb0bbe369ac1386adacc9bd4506499a4aa26f
m0nq <forgetmenaught@gmail.com>
2018-08-23 22:48:22 -0700

    Get a react dom element to clear the page of text when a button event triggers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
@@ -11,17 +12,24 @@ class App extends Component {
     };
 
     this.updateState = this.updateState.bind(this);
+    this.clearInput = this.clearInput.bind(this);
   }
 
   updateState(e) {
     this.setState({data: e.target.value});
   }
 
+  clearInput() {
+    this.setState({data: ''});
+    ReactDOM.findDOMNode(this.refs.myText).focus();
+  }
+
   render() {
     return (
       <div>
-        <input type="text" value={this.state.data} onChange={this.updateState}/>
+        <input type="text" value={this.state.data} onChange={this.updateState} ref="myText"/>
         <h4>{this.state.data}</h4>
+        <button onClick={this.clearInput}>Clear</button>
       </div>
     );
   }
